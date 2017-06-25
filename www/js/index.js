06197768648d49eec1bd6b75920ef52a06ac4964
behnam2019 <behnam2019@gmail.com>
2017-06-25 04:10:20 -0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,60 +1,61 @@
-function barcodescanner(){
+var World = {
+	loaded: false,
 
+	init: function initFn() {
+		this.createOverlays();
+	},
 
+	createOverlays: function createOverlaysFn() {
+		/*
+			First an AR.ImageTracker needs to be created in order to start the recognition engine. It is initialized with a AR.TargetCollectionResource specific to the target collection that should be used. Optional parameters are passed as object in the last argument. In this case a callback function for the onTargetsLoaded trigger is set. Once the tracker loaded all its target images, the function worldLoaded() is called.
 
- cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      },
-      {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : true, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: false, // Android, launch with the torch switched on (if available)
-          prompt : "بارکد را جلوی دوربین نگه دارید", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          //orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS
-      }
-   );
-    
-}
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+			Important: If you replace the tracker file with your own, make sure to change the target name accordingly.
+			Use a specific target name to respond only to a certain target or use a wildcard to respond to any or a certain group of targets.
+		*/
+        this.targetCollectionResource = new AR.TargetCollectionResource("assets/magazine.wtc", {
+        });
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        this.tracker = new AR.ImageTracker(this.targetCollectionResource, {
+            onTargetsLoaded: this.worldLoaded
+        });
 
-        console.log('Received Event: ' + id);
-    }
+		/*
+			The next step is to create the augmentation. In this example an image resource is created and passed to the AR.ImageDrawable. A drawable is a visual component that can be connected to an IR target (AR.ImageTrackable) or a geolocated object (AR.GeoObject). The AR.ImageDrawable is initialized by the image and its size. Optional parameters allow for position it relative to the recognized target.
+		*/
+
+		/* Create overlay for page one */
+		var imgOne = new AR.ImageResource("assets/imageOne.png");
+		var overlayOne = new AR.ImageDrawable(imgOne, 1, {
+			translate: {
+				x:-0.15
+			}
+
+		});
+
+		/*
+			The last line combines everything by creating an AR.ImageTrackable with the previously created tracker, the name of the image target and the drawable that should augment the recognized image.
+			Please note that in this case the target name is a wildcard. Wildcards can be used to respond to any target defined in the target collection. If you want to respond to a certain target only for a particular AR.ImageTrackable simply provide the target name as specified in the target collection.
+		*/
+		var pageOne = new AR.ImageTrackable(this.tracker, "*", {
+			drawables: {
+				cam: overlayOne
+			}
+		});
+	},
+
+	worldLoaded: function worldLoadedFn() {
+		var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
+		var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
+		document.getElementById('loadingMessage').innerHTML =
+			"<div" + cssDivLeft + ">Scan Target &#35;1 (surfer):</div>" +
+			"<div" + cssDivRight + "><img src='assets/surfer.png'></img></div>";
+
+		// Remove Scan target message after 10 sec.
+		setTimeout(function() {
+			var e = document.getElementById('loadingMessage');
+			e.parentElement.removeChild(e);
+		}, 10000);
+	}
 };
+
+World.init();
